b5912fbf2cfdd86851adbc41025d49f6168344ac
Timothy Stewart <timothy.stewart18@yahoo.com>
2020-04-14 20:32:01 -0500

    finished unit 1 - Flix app

---
+++ FlixMovie2/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  FlixMovie2
+//
+//  Created by Timothy Stewart on 3/5/20.
+//  Copyright © 2020 Timothy Stewart. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
