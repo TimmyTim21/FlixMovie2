9dbb352a853c66920f897799c3b85951da754e8b
Timothy Stewart <timothy.stewart18@yahoo.com>
2020-04-18 13:47:05 -0500

    Working on Flix app part 2. Having trouble with code. Unexpectedly found nil while unwrapping an Optional value» Error. The app runs but it crashes every time i had the tab bar .

---
+++ FlixMovie2/MovieGridCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieGridCell.swift
+//  FlixMovie2
+//
+//  Created by Timothy Stewart on 4/17/20.
+//  Copyright © 2020 Timothy Stewart. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
